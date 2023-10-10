66e787a250ba3c311a9693842406d7dacc8847a6
Mac Larsen <122307091+MacLarsen9@users.noreply.github.com>
2023-10-10 12:49:36 -0700

    Update email-notification.py
    created send_mail def

--- ED-Notification/email-notification.py
+++ ED-Notification/email-notification.py
@@ -1,29 +1,30 @@
-import os
 from email.message import EmailMessage
 import smtplib
 import json
+import time
 
 file_path = '/Users/maclarsen/cs/email_trial/mockDB.json'
 
 with open(file_path, 'r') as openfile:
     data = json.load(openfile)
 
-specific_object = data[0]
+def send_email(objects):
+    sender = 'mistered.health@gmail.com'
+    password = 'xquy owpn pqqe ctis'
+    receiver = objects['email']
 
-sender = 'mistered.health@gmail.com'
-password = 'xquy owpn pqqe ctis'
-receiver = specific_object['email']
+    subject = 'Mister Ed: Queue Update'
+    body = objects['name']+", you are next to receive treatment at " + objects['nearestED'] + ". Please travel there now."
 
-subject = 'Mister Ed: Queue Update'
-body = specific_object['name']+", you are next to receive treatment at " + specific_object['nearestED'] + ". Please travel there now."
+    time.sleep(objects['wait'])
 
-message = EmailMessage()
-message['From'] = sender
-message['To'] = receiver
-message['Subject'] = subject
-message.set_content(body)
+    message = EmailMessage()
+    message['From'] = sender
+    message['To'] = receiver
+    message['Subject'] = subject
+    message.set_content(body)
 
-with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-    smtp.starttls()
-    smtp.login(sender, password)
-    smtp.sendmail(sender, receiver, message.as_string())
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.starttls()
+        smtp.login(sender, password)
+        smtp.sendmail(sender, receiver, message.as_string())
