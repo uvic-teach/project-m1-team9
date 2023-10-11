1544137f6d89129a19d7e5990b6631169a604ffa
Stephen <45401615+stephenneale@users.noreply.github.com>
2023-10-11 22:03:59 +0000

    Add context starter

--- EDMapService/EDMapWebapp/views.py
+++ EDMapService/EDMapWebapp/views.py
@@ -1,9 +1,19 @@
 '''
 View classes building HTTP responses for URLS
 '''
+from typing import Any
 from django.views.generic import TemplateView
 
 
 class MapView(TemplateView):
     '''Main map page'''
     template_name = "map.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        '''Grabs data and sends to map.html'''
+        context = super().get_context_data(**kwargs)
+        coordinates = [[]]
+
+
+        context['coordinates'] = coordinates
+        return context
