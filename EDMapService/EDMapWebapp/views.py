33e14fd640669cdb2028ea97c66efc902f25f8da
Stephen Neale <stephenneale@uvic.ca>
2023-10-11 19:42:51 -0700

    Clean up context

--- EDMapService/EDMapWebapp/views.py
+++ EDMapService/EDMapWebapp/views.py
@@ -10,7 +10,7 @@ class MapView(TemplateView):
     '''Main map page'''
     template_name = "map.html"
 
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+    def get_context_data(self, **kwargs):
         '''Grabs data and sends to map.html'''
         context = super().get_context_data(**kwargs)
         coordinates = []
