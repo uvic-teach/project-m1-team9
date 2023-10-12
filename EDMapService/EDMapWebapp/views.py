26db58bee3b57e88721bb94d0e62ab01ce5d4abb
Stephen Neale <stephenneale@uvic.ca>
2023-10-11 19:37:35 -0700

    Mv to local, mv csv

--- EDMapService/EDMapWebapp/views.py
+++ EDMapService/EDMapWebapp/views.py
@@ -13,14 +13,19 @@ class MapView(TemplateView):
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         '''Grabs data and sends to map.html'''
         context = super().get_context_data(**kwargs)
-        coordinates = [[]]
+        coordinates = []
 
-        with open('EDMapService/EDMapData/HospitalList.csv',
+
+
+        # Open csv
+        with open('edcoordinates.csv',
                 newline='',
                 encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                print(row['hospitalname'], row['latitude'], row['longitude'])
+                coordinates.append([row['hospitalname'], row['latitude'], row['longitude']])
+            
+        print(coordinates)
 
         context['coordinates'] = coordinates
         return context
