421a91fcde9bdfb6d56b00f3c9dfab07da735c48
Mac Larsen <122307091+MacLarsen9@users.noreply.github.com>
2023-11-01 15:59:17 -0700

    Update email_notification.py

--- ED-Notification/email_notification.py
+++ ED-Notification/email_notification.py
@@ -1,26 +1,30 @@
 from email.message import EmailMessage
 import smtplib
 import json
-import time
 
-with open('mockDB.json', 'r') as openfile:        #access json file and saves objects as list to data
+file_path = "mockDB.json"
+
+with open(file_path, 'r') as openfile:
     data = json.load(openfile)
 
 def send_email(objects):
-    sender = 'mistered.health@gmail.com'  #address email sent from
-    password = 'xquy owpn pqqe ctis'      #password to send fro above address
-    receiver = objects['email']           #address of recipient (taken from json for mock or INSERT YOUR EMAIL HERE TO VIEW RESULTS FOR MARKING PURPOSES)
 
-    subject = 'Mister Ed: Queue Update'        #email subject header
-    body = objects['name']+", you are next to receive treatment at " + objects['nearestED'] + ".\n\nPlease travel there now."        #email body
+    if(objects['EDqueue'] == 0):
 
-    message = EmailMessage()
-    message['From'] = sender   
-    message['To'] = receiver
-    message['Subject'] = subject
-    message.set_content(body)
+        sender = 'mistered.health@gmail.com'  #email is sent from
+        password = 'xquy owpn pqqe ctis'  #password needed to access gmail to send email
+        receiver = objects['email']  #recipient of email (taken from json for mock)
 
-    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-        smtp.starttls()
-        smtp.login(sender, password)
-        smtp.sendmail(sender, receiver, message.as_string())
+        subject = 'Mister Ed: Queue Update' #subject of email
+        body = objects['name']+", you are next to receive treatment at " + objects['nearestED'] + ".\nPlease travel there now." #mail script
+
+        message = EmailMessage()
+        message['From'] = sender
+        message['To'] = receiver
+        message['Subject'] = subject
+        message.set_content(body)
+
+        with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+            smtp.starttls()
+            smtp.login(sender, password)
+            smtp.sendmail(sender, receiver, message.as_string())
