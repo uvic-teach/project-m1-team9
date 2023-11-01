1dcfe20f476dfd1588f2aa9794410b3d5189c38a
Mac Larsen <122307091+MacLarsen9@users.noreply.github.com>
2023-11-01 15:57:16 -0700

    Update mock_emails.py
    Added queue based emailing

--- ED-Notification/mock_emails.py
+++ ED-Notification/mock_emails.py
@@ -2,13 +2,17 @@ from email.message import EmailMessage
 import json
 from email_notification import send_email
 
+file_path = "mockDB.json"
+
 with open('mockDB.json', 'r') as openfile:;        #open json file and save objects to data as list
     data = json.load(openfile)
 
 def main():
-    for objects in data:
-        time.sleep(15)        #mimic time between receiving updates from physician service
-        send_email(objects)
+    while(1):
+        for objects in data:
+            send_email(objects)
+            if(objects['EDqueue']>=0):
+                objects['EDqueue'] = objects['EDqueue']-1
 
 if __name__ == "__main__":
     main()
