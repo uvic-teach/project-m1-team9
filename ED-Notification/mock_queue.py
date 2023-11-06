4b91411e3af58c39f477e90da4f185a7eea9bb0b
er-ber-ter <122562633+er-ber-ter@users.noreply.github.com>
2023-11-06 14:20:40 -0800

    Create mock_queue.py
    Created the mock_queue.py file and added comments outlining plan (Discussed heavily with Brendan)

---
+++ ED-Notification/mock_queue.py
@@ -0,0 +1,9 @@
+# This is a mock database with some assumptions
+# 1: Queue must run independently (not controlled by anything else)
+# 2: Queue must have an interface to interact with that enables:
+#   Interface to add to queue (M3: load (mockDB(seperatefile) + mockNoise(hardcoded))) (possible shuffle), key is name (M3: assumption no two patients have the same name),
+#   print list of users (debug purposes super simple m3) 
+#   TO BE TREATED LIST (TBTL) -> implements kick (M4: Kick cond. 1: Timeout 2: Confirm treated)
+#   TBTL.kick(patient) -> remove patient from TBTL, then dequeue item on queue and put into TBTL
+#   for now M3; have kick place patient at end of queue (infinite loop)
+
