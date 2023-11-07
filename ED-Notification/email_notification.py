70eadbd90581f5284196c3820b49218070202bd9
Mac Larsen <122307091+MacLarsen9@users.noreply.github.com>
2023-11-06 21:42:13 -0800

    Implemented Factory design pattern
    Return to previous commit to remove implementation

--- ED-Notification/email_notification.py
+++ ED-Notification/email_notification.py
@@ -2,29 +2,41 @@ from email.message import EmailMessage
 import smtplib
 import json
 
-file_path = "mockDB.json"
-
-with open(file_path, 'r') as openfile:
-    data = json.load(openfile)
-
-def send_email(objects):
-
-    if(objects['EDqueue'] == 0):
-
-        sender = 'mistered.health@gmail.com'  #email is sent from
-        password = 'xquy owpn pqqe ctis'  #password needed to access gmail to send email
-        receiver = objects['email']  #recipient of email (taken from json for mock)
-
-        subject = 'Mister Ed: Queue Update' #subject of email
-        body = objects['name']+", you are next to receive treatment at " + objects['nearestED'] + ".\nPlease travel there now." #mail script
-
+class EmailFactory:
+    def create_email(self, sender, password, receiver, subject, body):
         message = EmailMessage()
         message['From'] = sender
         message['To'] = receiver
         message['Subject'] = subject
         message.set_content(body)
+        return message
+
+    def send_email(self, sender, password, receiver, subject, body):
+        message = self.create_email(sender, password, receiver, subject, body)
 
         with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
             smtp.starttls()
             smtp.login(sender, password)
             smtp.sendmail(sender, receiver, message.as_string())
+
+def main():
+    file_path = "mockDB.json"
+    
+    with open(file_path, 'r') as openfile:
+        data = json.load(openfile)
+    
+    def send_email(objects):
+    
+        if(objects['EDqueue'] == 0):
+    
+            sender = 'mistered.health@gmail.com'  #email is sent from
+            password = 'xquy owpn pqqe ctis'  #password needed to access gmail to send email
+            receiver = objects['email']  #recipient of email (taken from json for mock)
+            subject = 'Mister Ed: Queue Update' #subject of email
+            body = objects['name']+", you are next to receive treatment at " + objects['nearestED'] + ".\nPlease travel there now." #mail script
+    
+            email_factory = EmailFactory()
+            email_factory.send_email(sender, password, receiver, subject, body)
+
+if __name__ == "__main__":
+    main()
