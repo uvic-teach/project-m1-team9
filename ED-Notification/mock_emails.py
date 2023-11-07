9f8680c13c677635f47128fd3b00a6fcc272723c
Mac Larsen <122307091+MacLarsen9@users.noreply.github.com>
2023-11-06 16:01:06 -0800

    Increased readability

--- ED-Notification/mock_emails.py
+++ ED-Notification/mock_emails.py
@@ -10,21 +10,28 @@ with open('mockDB.json', 'r') as openfile:;        #open json file and save obje
 
 def main():
     while(1):
+        
         with open(file_path, 'r') as openfile:  #read json file (in loop as new users may be added while running)
             data = json.load(openfile)
+            
         for objects in data:        #iterates through each user
             send_email(objects)     #calls email function from file
+           
             if(objects['EDqueue']>=0):    
                 objects['EDqueue'] = objects['EDqueue']-1  #moves user up queue as long as they arent already at the front
+        
         time.sleep(1) #slows down mock to determine incoming email order
+        
                                                 ###Uncomment below to add a user while the program is running###
         #i = i+1
         #if i==10:
         #    data.append({"name": "Mary", "email": "<INSERT YOUR EMAIL HERE>", "nearestED": "Oak Bay Urgent Care Clinic", "EDqueue": 10})
         #   with open(file_path, 'w') as openfile:
         #        json.dump(data, openfile)
+        
         with open(file_path, 'w') as openfile:  #update json file (in loop as new users may be added while running
             json.dump(data, openfile)
+            
         time.sleep(1) #slows down mock to determine incoming email order and ensure that JSON is not empty following mock
 
 if __name__ == "__main__":
