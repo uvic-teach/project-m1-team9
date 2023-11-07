ac8b2269142db16f0be537d49be6886ee796031c
Mac Larsen <122307091+MacLarsen9@users.noreply.github.com>
2023-11-06 21:35:19 -0800

    Updated so that email_notification.py will run using Factory Pattern
    Return to previous commit to revert to before pattern design implementation

--- ED-Notification/mock_emails.py
+++ ED-Notification/mock_emails.py
@@ -1,11 +1,12 @@
 from email.message import EmailMessage
 import json
-from email_notification import send_email
+from email_notification import EmailFactory
 import time
 
 ##########################################################################
 
 #run this python file to test/mock email_notification.py
+#this program will take up to around an entire minute to for all emails to be sent and will continue to run until manually terminated
 
 #ensure that <INSERT YOUR EMAIL> in this file and in mockDB.json 
 #are changed to the email address that you want this message sent to
@@ -18,13 +19,17 @@ with open('mockDB.json', 'r') as openfile:;        #open json file and save obje
     data = json.load(openfile)
 
 def main():
-    while(1):
+
+#i = 0                     ###Uncomment to add a user while the program is running, uncomment the commented code below as well
+    
+    while True:
         
         with open(file_path, 'r') as openfile:  #read json file (in loop as new users may be added while running)
             data = json.load(openfile)
             
         for objects in data:        #iterates through each user
-            send_email(objects)     #calls email function from file
+            email_factory.send_email(sender = 'mistered.health@gmail.com', password = 'xquy owpn pqqe ctis', receiver = objects['email'], subject = 'Mister Ed: Queue Update', body = objects['name'] + ", you are next to receive treatment at " + objects['nearestED'] + ".\nPlease travel there now.")
+     #calls email function from file
            
             if(objects['EDqueue']>=0):    
                 objects['EDqueue'] = objects['EDqueue']-1  #moves user up queue as long as they arent already at the front
