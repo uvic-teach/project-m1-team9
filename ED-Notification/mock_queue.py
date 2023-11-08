639fda3a04922cb56339a433a9171d6147da62f1
er-ber-ter <122562633+er-ber-ter@users.noreply.github.com>
2023-11-08 15:25:46 -0800

    Update mock_queue.py
    Implemented kick() in TBTL object, updated add() function override in TBTL, other class changes and added comments.

--- ED-Notification/mock_queue.py
+++ ED-Notification/mock_queue.py
@@ -2,9 +2,10 @@
 # 1: Queue must run independently (not controlled by anything else)
 # 2: Queue must have an interface to interact with that enables:
 #   Interface to add to queue (M3: load (mockDB(seperatefile) + mockNoise(hardcoded))) (possible shuffle), key is name (M3: assumption no two patients have the same name),
-#   print list of users (debug purposes super simple m3) 
-#   TO BE TREATED LIST (TBTL) -> implements kick (M4: Kick cond. 1: Timeout 2: Confirm treated)
-#   TBTL.kick(patient) -> remove patient from TBTL, then dequeue item on queue and put into TBTL
+#   (COMPLETE) print list of users (debug purposes super simple m3) 
+#   (COMPLETE) TO BE TREATED LIST (TBTL) -> implements kick 
+#   (M4: Kick cond. 1: Timeout 2: Confirm treated)
+#   (COMPLETE) TBTL.kick(patient) -> remove patient from TBTL, then dequeue item on queue and put into TBTL
 #   for now M3; have kick place patient at end of queue (infinite loop)
 
 import json
@@ -27,7 +28,8 @@ class ED_List:
   
   def remove(self, key):
     for key in self.list:
-      self.list.remove(key)
+      item = self.list.remove(key)
+      return item
 
   def size(self):
     return len(self.list)
@@ -39,7 +41,10 @@ class ED_List:
 class Queue(ED_List):
   def __init__(self):
     super.__init__()
-  
+    
+  # enqueue function is not meant to be a duplicate of the add() function of ED_lists
+  # future implementation would implement a smarter insertion of elements into the queue
+  # future implementation goal is to use smarter insertion to implement a priority queue
   def enqueue(self, patient):
     self.list.append(patient)
   
@@ -49,11 +54,27 @@ class Queue(ED_List):
     else:
       return None
 
-class TBTL:
-  def __init__(self):
+class TBTL(ED_list):
+  def __init__(self, capacity):
     super.__init__()
-    self.capacity = 3
+    self.capacity = capacity
+    self.queue = Queue()
   
+  def add(self, item):
+    if len(self.list) >= capacity:
+      print("TBTL is full");
+    else:
+      self.list.append(item)
+  
+  def kick(self, key):
+    # if infinite loop of patients isn't required, remove the patient variable
+    patient = self.remove(key)
+    # temporary infinite loop of rotating patients for development/demonstration purposes
+    self.queue.enqueue(patient)
+    
+    patient = self.queue.dequeue()
+    if patient != None:
+      self.add(patient)
 
 
 def main():
