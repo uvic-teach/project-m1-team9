dba5455fbaabe81e2e05321096e627d3dfc9f20b
Brendon Waters <brendonwaters@uvic.ca>
2023-11-12 19:54:30 -0800

    Implemented UpdateUserById and CreateUser endpoints

--- EDUserManagementService/EDUserManagementApp/urls.py
+++ EDUserManagementService/EDUserManagementApp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from . import views
-from .views import GetUserById
+from .views import GetUserById, UpdateUserById, CreateUser
 
 urlpatterns = [
-    path('user/<int:pk>/', GetUserById.as_view(), name='get-user-by-id'),
     path("", views.index, name="index"),
+    path('user/<int:pk>/', GetUserById.as_view(), name='get-user-by-id'),
+    path('user/<int:pk>/update/', UpdateUserById.as_view(), name='update-user-by-id'),
+    path('user/create/', CreateUser.as_view(), name='create-user'),
 ]
--- EDUserManagementService/EDUserManagementApp/views.py
+++ EDUserManagementService/EDUserManagementApp/views.py
@@ -1,12 +1,28 @@
 from django.http import HttpResponse
 from rest_framework import generics
 from .models import CustomUser
-from .serializers import CustomUserSerializer
+from .serializers import CustomUserGetSerializer, CustomUserPatchSerializer, CustomUserPutSerializer
 
 class GetUserById(generics.RetrieveAPIView):
     queryset = CustomUser.objects.all()
-    serializer_class = CustomUserSerializer
+    serializer_class = CustomUserGetSerializer
     lookup_field = 'pk'  # This tells Django to use 'pk' as the lookup field
+
+    # Optionally, you can add authentication and permission classes here
+
+class GetUserByPhn(generics.RetrieveAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserGetSerializer
+    lookup_field = 'phn'  # This tells Django to use 'phn' as the lookup field
+
+    # Optionally, you can add authentication and permission classes here
+
+class UpdateUserById(generics.UpdateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserPatchSerializer
+
+class CreateUser(generics.CreateAPIView):
+    serializer_class = CustomUserPutSerializer
 
     # Optionally, you can add authentication and permission classes here
 
