a53a13606a693ad69d8cf57452d9d62376a1714f
Brendon Waters <brendonwaters@uvic.ca>
2023-12-15 15:22:08 -0800

    Implemented basic token authentication for UserMan endpoints. Updated README with documentation, testing and contribution info:

--- EDUserManagementService/EDUserManagementService/settings.py
+++ EDUserManagementService/EDUserManagementService/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'EDUserManagementApp.middleware.token_auth_middleware',
 ]
 
 ROOT_URLCONF = 'EDUserManagementService.urls'
